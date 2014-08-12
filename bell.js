7b91cccb4bc0671ed39e68a62164c8bffff7e116
hit9 <nz2324@126.com>
2014-08-12 14:06:35 +0800

    init structure

---
+++ bell.js
@@ -0,0 +1,63 @@
+// Copyright (c) 2014 Eleme, Inc. https://github.com/eleme/node-bell
+//
+// Usage: bell <service> [options]
+//
+// Options:
+//
+//   -h, --help            output usage information
+//   -V, --version         output the version number
+//   -c, --configs <c>     configs file path
+//   -l, --log-level <l>   logging level (1~5 for debug~critical)
+//   -s, --sample-configs  generate sample configs
+
+var co = require('co')
+  , path = require('path')
+  , program = require('commander')
+  , toml = require('toml')
+  , configs = require('./lib/configs')
+  , listener = require('./lib/listener')
+  , util = require('./lib/util');
+
+var log = util.log;
+
+
+function parseLogLevel(val) {
+  return (parseInt(val) - 1) % 5  + 1;  // limit to 1~5
+}
+
+
+co(function *(){
+  program
+    .version('0.0.1')
+    .usage('<service> [options]')
+    .option('-c, --configs-path <c>', 'configs file path')
+    .option('-l, --log-level <l>', 'logging level (1~5 for critical~debug)', parseLogLevel)
+    .option('-s, --sample-configs', 'generate sample configs file')
+    .parse(process.argv);
+
+  // set log level
+  log.level = util.logLevels[program.logLevel || 4];
+
+  // generate sample configs file
+  if (program.sampleConfigs) {
+    util.copy(util.path.configs, 'sample.configs.toml');
+    log.info('Generate sample.configs.toml done.')
+    return
+  }
+
+  // update configs
+  var configsPath = program.configsPath || util.path.configs;
+  var content = yield util.readFile(configsPath)
+  util.updateNestedObjects(configs, toml.parse(content.toString()));
+
+  // reterve service name
+  var name = program.args[0];
+  if (!name) program.help();
+
+  var service = {
+    listener: listener
+  }[name];
+
+  if (!service) program.help();
+  else service.serve();
+})();
---
+++ bin/cli.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node --harmony-generators
+
+require('../bell.js');
