1b9ac64fb4987d8285901872a00a98e046cf6c5f
hit9 <nz2324@126.com>
2014-08-20 16:05:54 +0800

    Do Not share ssdb and app across threads

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ for periodic time series.
 
 ![](https://github.com/eleme/node-bell/raw/master/snap.png)
 
-Latest version: v0.1.2
+Latest version: v0.1.3
 
 Requirements
 ------------
--- bell.js
+++ bell.js
@@ -46,7 +46,7 @@ var co = require('co')
 
 co(function *(){
   program
-  .version('0.1.2')
+  .version('0.1.3')
   .usage('<service> [options]')
   .option('-c, --configs-path <c>', 'configs file path')
   .option('-s, --sample-configs', 'generate sample configs file')
