394e4c706a14f63fd939a940814e57cff95ea0f4
hit9 <nz2324@126.com>
2014-08-22 19:19:26 +0800

    restructured

--- static/js/script.js
+++ static/js/script.js
@@ -1,98 +1,97 @@
-function init(pattern, limit, apiPrefix) {
-  /*
-   * cubism context
-   */
+(function(){
   var context = cubism.context()
   .serverDelay(0)
   .clientDelay(0)
   .step(1e4)  // 10sec
-  .size(1080)  // 3h
+  .size(1080) // 4h
   ;
 
-  /*
-   * metrics source
-   */
-  function pullMetrics(name) {
-    return context.metric(function(start, stop, step, callback){
-      // cast to timestamp
-      start = +start/1000, stop = +stop/1000, step = +step/1000;
-      // api url to fetch metrics
-      var api = apiPrefix + '/' + ['metrics', name, start, stop].join('/');
-      var values = [], i = 0;
-      // send request
-      var xmlhttp=new XMLHttpRequest();
-      xmlhttp.open("GET", api, true);
-      xmlhttp.send();
-      xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-          var data = JSON.parse(xmlhttp.responseText);
+  var pattern, limit, type, api;
 
-          while (start < stop) {
-            while (start < data.times[i]) {
-              start += step;
-              values.push(0);
-            }
-            values.push(data.vals[i++]);
-            start += step;
-          }
-          callback(null, values);
-        }
-      };
-    }, name);
-  }
+  this.initBell = function(t, p, l, a) {
+    type = t;
+    pattern = p;
+    limit = l;
+    api = a;
+
+    plot();
+  };
 
   /*
-   * names source
+   * 'GET' request an url, and call callback with JSON data
    */
-  function pullNames(callback) {
-    var api = apiPrefix + '/' + ['names', pattern, limit].join('/');
-    var xmlhttp=new XMLHttpRequest();
-    xmlhttp.open("GET", api, true);
+  function request(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', url, true);
     xmlhttp.send();
     xmlhttp.onreadystatechange = function() {
       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var names = JSON.parse(xmlhttp.responseText);
-        callback(names);
+        var data = JSON.parse(xmlhttp.responseText);
+        callback(data);
       }
     };
   }
 
+
   /*
-   * plot graph
+   * Metrcs source
    */
+  function makeMetric(name) {
+    return context.metric(function(start, stop, step, callback){
+      // cast to timestamp from date
+      start = +start / 1000;
+      stop = +stop / 1000;
+      step = +step / 1000;
 
-  function plot() {
-    pullNames(function(names){
-      var d = [];
-      for (var i = 0; i < names.length; i++) {d.push(pullMetrics(names[i]))};
+      // api url to fetch metrics
+      var url = [api, 'metrics', name, start, stop].join('/');
+      var values = [], i = 0;
 
-      d3.select("#chart").call(function(div) {
+      // request data and call callback with values
+      request(url, function(data) {
+        while (start < stop) {
+          while (start < data.times[i]) {
+            start += step;
+            values.push(0);  // push 0 if no data at this timestamp
+          }
+          values.push(data.vals[i++]);
+          start += step;
+        }
+        callback(null, values);
+      });
+    }, name);
+  }
 
-        div.append("div")
-        .attr("class", "axis")
-        .call(context.axis().orient("top"));
+  /*
+   * plot
+   */
+  function plot () {
+    var url = [api, 'names', pattern, limit].join('/');
 
-        div.selectAll(".horizon")
-        .data(d)
-        .enter().append("div")
-        .attr("class", "horizon")
-        .call(
-          context.horizon()
-          .extent([0, 2])
-          .colors(['black', 'black', 'teal', '#dd1144'])
-        );
+    request(url, function(names){
+      var data = [];
+      for (var i = 0; i < names.length; i++) {
+        data.push(makeMetric(names[i]));
+      }
 
-        div.append("div")
-        .attr("class", "rule")
+      d3.select('#chart').call(function(div) {
+        div.append('div')
+        .attr('class', 'axis')
+        .call(context.axis().orient('top'));
+
+        div.selectAll('.horizon')
+        .data(data)
+        .enter().append('div')
+        .attr('class', 'horizon')
+        .call(context.horizon()
+              .extent([0, 2])
+              .colors(['black', 'black', 'teal', '#dd1144']));
+
+        div.append('div')
+        .attr('class', 'rule')
         .call(context.rule());
       });
     });
   }
 
-  plot();
-
-  setInterval(function(){
-    d3.select("#chart").selectAll("*").remove();
-    plot();
-  }, 5 * 60 * 1000);  // replot every 5 min
-}
+})(this);
